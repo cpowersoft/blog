266850e1eed486017bfd4a8799d0f1b94627a8f6
junwei.wu <nick.wu@patpat.com>
2017-08-19 16:34:29 +0800

    update

--- config/blog.php
+++ config/blog.php
@@ -34,7 +34,7 @@ return [
     // Article Page
     'article' => [
         'title'       => 'Nothing is impossible.',
-        'description' => 'https://pigjian.com',
+        'description' => 'http://www.93myb.com',
         'number'      => 15,
         'sort'        => 'desc',
         'sortColumn'  => 'published_at',
