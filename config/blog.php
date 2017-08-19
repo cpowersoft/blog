9d675fa104606e63d5a5d51a48124c74f6badeb9
junwei.wu <nick.wu@patpat.com>
2017-08-19 16:35:52 +0800

    update

--- config/blog.php
+++ config/blog.php
@@ -56,6 +56,6 @@ return [
         'meta' => '© Blog 2017. Powered By Nick Ng',
     ],
 
-    'license' => 'Powered By Jiajian Chan.<br/>This article is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/">Creative Commons Attribution-NonCommercial 4.0 International License</a>.',
+    'license' => 'Powered By Nick Ng.',
 
 ];
